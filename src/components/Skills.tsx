c78a0ab7f9e42d037f42da965806e1ffeea796ac
hastalavista99 <githumuj@gmail.com>
2025-11-01 07:39:25 +0300

    completed skills section

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,7 +1,21 @@
+import { Badge } from "./ui/badge";
+import skills from "@/constants/skills";
+
 export default function Skills() {
     return (
-        <section>
-            Skills Section
+        <section className="flex flex-col items-center justify-center" id="skills">
+            <Badge variant="secondary">Skills</Badge>
+            <p className="mt-4">The skills, tools and technologies I am really good at:</p>
+            <div className="mt-12">
+                <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-6 lg:grid-cols-8 gap-x-24 gap-y-12 ">
+                    {skills.map((skill) => (
+                        <li key={skill.name} className="flex flex-col items-center gap-2">
+                            <img src={skill.icon} alt={skill.name} className="w-12 h-12 object-contain" />
+                            <span className="text-sm">{skill.name}</span>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </section>
     )
 }
