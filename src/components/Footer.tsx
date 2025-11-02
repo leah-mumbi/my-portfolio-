b574a624401684028dbb63a46118634029d7e163
hastalavista99 <githumuj@gmail.com>
2025-11-02 09:32:09 +0300

    Added footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
-        <footer>
-            Footer Section
+        <footer className="bg-gray-50 text-center py-6">
+            &copy; 2023 | Designed and coded with ❤️️ by Sagar Shah
         </footer>
     )
 }
